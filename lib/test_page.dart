0c60b61587b691f8664a319dc1688f27e65158e6
Ananda S <4833102+asarangaram@users.noreply.github.com>
2023-11-30 15:02:21 +0530

    clean up excessive comments from the template

---
+++ lib/test_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TestPage extends StatelessWidget {
+  const TestPage({super.key, required this.title});
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: const Center(
+        child: Text(
+          'Test Page',
+        ),
+      ),
+    );
+  }
+}
